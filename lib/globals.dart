df3845d53317c18c2996c07fa93584d7367aedcb
czyeet1226 <114337055+czyeet1226@users.noreply.github.com>
2025-07-21 23:18:30 +0800

    Create globals.dart

---
+++ lib/globals.dart
@@ -0,0 +1,10 @@
+// globals.dart
+library my_project.globals;
+
+String globalEmail = '';
+String globalPassword = '';
+String globalRole = '';
+String globalName = '';
+String globalPhone = '';
+String globalIC = '';
+String globalGender = '';
