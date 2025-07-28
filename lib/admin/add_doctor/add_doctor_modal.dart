af622e3e5f5d22e95723b7300232e2dda8246ec1
czyeet1226 <114337055+czyeet1226@users.noreply.github.com>
2025-07-28 11:44:50 +0800

    Create add_doctor_modal.dart

---
+++ lib/admin/add_doctor/add_doctor_modal.dart
@@ -0,0 +1,11 @@
+import '/flutter_flow/flutter_flow_util.dart';
+import 'add_doctor_widget.dart' show AddDoctorWidget;
+import 'package:flutter/material.dart';
+
+class AddDoctorModel extends FlutterFlowModel<AddDoctorWidget> {
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
