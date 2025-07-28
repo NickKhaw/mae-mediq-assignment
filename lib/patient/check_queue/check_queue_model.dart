0b0c55bb1fbad24b2cd0ce8d99e6744a9325975c
czyeet1226 <114337055+czyeet1226@users.noreply.github.com>
2025-07-28 11:23:26 +0800

    Create check_queue_model.dart

---
+++ lib/patient/check_queue/check_queue_model.dart
@@ -0,0 +1,22 @@
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/form_field_controller.dart';
+import 'check_queue_widget.dart' show CheckQueueWidget;
+import 'package:flutter/material.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
+
+class CheckQueueModel extends FlutterFlowModel<CheckQueueWidget> {
+  ///  State fields for stateful widgets in this page.
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void initState(BuildContext context) {
+    // TODO: Add any initialization code here
+  }
+
+  @override
+  void dispose() {
+    // TODO: Dispose any controllers here
+  }
+}
+
+  
